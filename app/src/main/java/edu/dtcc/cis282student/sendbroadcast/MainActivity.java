b50864317e68e506d2ed22a243a29e9ce53fa320
cis282student <mmendez9@dtcc.edu>
2017-01-31 12:08:57 -0500

    Broadcast

---
+++ app/src/main/java/edu/dtcc/cis282student/sendbroadcast/MainActivity.java
@@ -0,0 +1,22 @@
+package edu.dtcc.cis282student.sendbroadcast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnBroadcastClick(View view) {
+        Intent intent = new Intent();
+        intent.setAction("edu.dtcc.cis282student.sendbroadcast");
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(intent);
+    }
+}
